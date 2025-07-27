10e6b4dacdb952a7e54dd82d435d627d1bd02a12
jeffc0985 <a0985342880@gmail.com>
2025-07-28 04:38:00 +0800

    Add files via upload

---
+++ L3/3.1/3.1.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 12400;
+    double b = 5.234;
+
+    printf("a to the power of two is %d\n",a*a);
+    printf("b to the power of two is %f\n",b*b);
+    return 0;
+} 
---
+++ L3/3.2/3.2.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("123\n");
+    printf("123\b\n");
+    printf("123\t\t");
+    printf("123\r");
+    printf("123\a");
+    return 0;
+}
